f03cf794b33da56cf06b4252fa5bdf23ab3158b9
LPreche <paulorecheme@hotmail.com>
2023-09-25 15:59:11 -0300

    Style: Ajustado os arquivos para ficar separado o estilo responsivo

--- src/app/components/grid-card/grid-card.component.ts
+++ src/app/components/grid-card/grid-card.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-grid-card',
   templateUrl: './grid-card.component.html',
-  styleUrls: ['./grid-card.component.css']
+  styleUrls: [
+    './grid-card.component.css',
+    './grid-card.responsive.component.css'
+  ]
 })
 export class GridCardComponent implements OnInit {
 
