c4c2cf95d5cf6c9f206d3ba47a4363e025bc4c7f
LPreche <paulorecheme@hotmail.com>
2023-09-28 22:27:29 -0300

    Feat: implementado a busca dimanica dos dados da noticia, simulando dados vindo de um arquivo em formato JSON

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,8 +1,87 @@
 export const dataFake =  [
     {
-        "id":"1",
-        "image":"https://i0.wp.com/news.xbox.com/pt-br/wp-content/uploads/sites/8/2020/09/BethesdaXbox_HERO.jpg?resize=1200%2C675&ssl=1",
-        "title":"Compra da Bethesda pela Microsoft",
-        "description":"Agora é para valer! A compra da Bethesda pela Microsoft foi aprovada pelos órgãos reguladores responsáveis nos EUA e na Europa – United States Securities and Exchange Commission e European Commission, respectivamente. A aquisição foi anunciada em outubro de 2020, com valor informado de US$ 7,5 bilhões. Em fevereiro, a Microsoft chegou a criar uma nova subsidiária chamada Vault, para mesclar à ZeniMax, detentora da Bethesda e de outros estúdios parceiros. O nome é uma clara referência às Vaults encontradas na série Fallout, que são abrigos. A inclusão mostrou que a empresa já estava preparada para a finalização do negócio, que agora não tem impedimentos legais."
+        "id": "1",
+        "title": "Compra da Bethesda pela Microsoft",
+        "image": "https://i0.wp.com/news.xbox.com/pt-br/wp-content/uploads/sites/8/2020/09/BethesdaXbox_HERO.jpg?resize=1200%2C675&ssl=1",
+        "description": "A compra da Bethesda pela Microsoft já dura 3 anos. Os EUA já aprovaram a transação, mas ainda falta a aprovação de alguns países europeus."
+    },
+    {
+        "id": "2",
+        "title": "Lançamento de Starfield foi um sucesso!",
+        "image": "https://xboxpower-wp.s3.amazonaws.com/wp-content/uploads/2023/08/starfield-splash-featured.jpg",
+        "description": "O lançamento de Starfield foi aclamado como um sucesso, com os jogadores elogiando os visuais e a jogabilidade do jogo."
+    },
+    {
+        "id": "3",
+        "title": "Microsoft lança Xbox Series S com 1 TB de armazenamento",
+        "image": "https://s2-techtudo.glbimg.com/NMbsHSH5vONIALxK2_LcpdtMIsM=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/u/B/5l2wPuSDe1gdQvHrXfwg/beauty-96c75b7e9904dba0b787.jpg",
+        "description": "A Microsoft lançou o Xbox Series S, uma versão mais acessível do console com 1 TB de armazenamento, tornando-o uma escolha popular entre os jogadores."
+    },
+    {
+        "id": "4",
+        "title": "State of Decay 3, Anunciado mas esquecido, o que houve com esse jogo promissor",
+        "image": "https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=910&height=568&format=webp&quality=91&imagick=uploads.adrenaline.com.br/2023/06/State-of-Decay-3-e-Perfect-Dark-devem-ganhar-mais-detalhes-nos-proximos-18-meses-912x569.jpg",
+        "description": "State of Decay 3 foi anunciado com grande empolgação, mas desde então, informações sobre o jogo têm sido escassas. Os fãs estão ansiosos para saber mais sobre o destino deste jogo promissor."
+    },
+    {
+        "id": "1G",
+        "title": "PAYDAY 3",
+        "image": "https://meups.com.br/wp-content/uploads/2023/08/PAYDAY-3.jpg",
+        "description": "PAYDAY 3 é um emocionante jogo de assalto em equipe, onde os jogadores planejam e executam roubos épicos."
+    },
+    {
+        "id": "2G",
+        "title": "CYBERPUNK 2077: PHANTOM LIBERTY",
+        "image": "https://flowgames.gg/wp-content/uploads/2023/06/EGS_Cyberpunk2077PhantomLiberty_CDPROJEKTRED_DLC_S1_2560x1440-15e79640f240b1652ad72c0ff72b2b17.jpg",
+        "description": "CYBERPUNK 2077: PHANTOM LIBERTY é uma expansão empolgante para o famoso jogo de RPG, trazendo novas aventuras futurísticas."
+    },
+    {
+        "id": "3G",
+        "title": "HAVERST MOON: THE WINDS OF ANTHOS",
+        "image": "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000053606/39d52fc510464498ee2c40c297ca8aa96e40454e27a3bc7ae0a6feaa9c4f41a6",
+        "description": "HAVERST MOON: THE WINDS OF ANTHOS é um encantador jogo de simulação agrícola com um mundo mágico para explorar."
+    },
+    {
+        "id": "4G",
+        "title": "Terminator Resistance Complete Edition",
+        "image": "https://www.ultimaficha.com.br/wp-content/uploads/2023/07/Terminator-Resistance-Complete-Edition-Ann_07-26-23-768x432-1.jpg",
+        "description": "Terminator Resistance Complete Edition traz a experiência completa de combate contra máquinas mortais no universo de Terminator."
+    },
+    {
+        "id": "5G",
+        "title": "EA FC 24",
+        "image": "https://image.api.playstation.com/vulcan/ap/rnd/202307/0710/28b62abf77fde8a35e133f69a9a253e4307b2a04d2040699.jpg?w=780&thumb=false",
+        "description": "EA FC 24 apresenta um conjunto diversificado de esportes e competições para os fãs de esportes eletrônicos."
+    },
+    {
+        "id": "6G",
+        "title": "WARHAMMER 40K DARKTIDE",
+        "image": "https://assets.xboxservices.com/assets/b6/b3/b6b31872-b43b-42ca-889e-b22e7fbe995f.jpg?n=Warhammer-40K-Darktide_GLP-Page-Hero-1084_1920x1080.jpg",
+        "description": "WARHAMMER 40K DARKTIDE leva os jogadores para o sombrio universo de Warhammer, onde enfrentarão hordas de inimigos monstruosos."
+    },
+    {
+        "id": "7G",
+        "title": "ASSASSINS CREED MIRAGE",
+        "image": "https://image.api.playstation.com/vulcan/ap/rnd/202208/1718/M1M5lbWg6QPwf2gx34N3v78v.jpg",
+        "description": "ASSASSINS CREED MIRAGE é a mais recente adição à popular série de jogos de ação e aventura, com reviravoltas intrigantes na história."
+    },
+    {
+        "id": "8G",
+        "title": "FORZA MOTORSPORT",
+        "image": "https://flowgames.gg/wp-content/uploads/2023/05/Fw_gl0DWcAA1CNt-scaled.jpeg",
+        "description": "FORZA MOTORSPORT traz uma experiência de corrida de alta velocidade com gráficos impressionantes e uma variedade de carros."
+    },
+    {
+        "id": "9G",
+        "title": "LORDS OF THE FALLEN",
+        "image": "https://cdn1.epicgames.com/offer/f2d5f78997994c9891b8f23d962be28e/EGS_LordsoftheFallen_HEXWORKS_S1_2560x1440-6e51b07edc72e1cec930eac05c86fdbc",
+        "description": "LORDS OF THE FALLEN é um jogo de ação e RPG com combate desafiador e um mundo sombrio para explorar."
+    },
+    {
+        "id": "10G",
+        "title": "CITIES SKYLINES 2",
+        "image": "https://sm.ign.com/t/ign_br/news/c/cities-sky/cities-skylines-2-revealed-for-current-gen-consoles-coming-l_ydxw.1280.jpg",
+        "description": "CITIES SKYLINES 2 é a próxima geração do popular simulador de cidade, oferecendo um controle completo sobre o planejamento urbano."
     }
+         
 ]
